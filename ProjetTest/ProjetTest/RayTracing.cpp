15a3d68d24ea07097d74cfe18bfe5332bd863d43
Lupusanghren <brisingr34130@gmail.com>
2019-10-05 13:32:16 +0200

    On a une classe Objet et une classe phere
    intersect est devenu une fonction abstraite de Object
    Maid ca ne marche plus a voir pk

--- ProjetTest/ProjetTest/RayTracing.cpp
+++ ProjetTest/ProjetTest/RayTracing.cpp
@@ -15,45 +15,14 @@ std::vector<Sphere> tabSphere;
 std::vector<Lumiere> tabLumiere;
 int nbMaxVecteurIndirect = 1;
 
-std::optional<float> RayTracing::intersect(Rayon R, Sphere S) {
-	float a = 1.f;
-	float b = 2 * (dot(R.origin, R.direction) - dot(S.centre, R.direction));
-	float c = dot(R.origin, R.origin) + dot(S.centre, S.centre) - 2 * dot(S.centre, R.origin) - S.rayon * S.rayon;
-	float delta = b * b - 4 * a * c;
-
-	if (delta > 0) {
-		float sol1 = (-b - std::sqrt(delta)) / (2 * a);
-		float sol2 = (-b + std::sqrt(delta)) / (2 * a);
-
-		if (sol1 >= 0.f) {
-			return sol1;
-		}
-		else if (sol2 >= 0.f) {
-			return sol2;
-		}
-		else {
-			//t=sol2;
-			//return sol2 > 0.f;
-			return std::nullopt;
-		}
-	}
-	else if (delta == 0) {
-		float sol = (-b / 2 * a);
-		return sol;
-	}
-	else {
-		return std::nullopt;
-	}
-}
-
-Vec3<float> kesseKisePazeOBouDutRaillon(Rayon ray, int profondeur) {
+Vec3<float> RayTracing::kesseKisePazeOBouDutRaillon(Rayon ray, int profondeur) {
 	if (profondeur > 5) {
 		return Vec3<float>{0.f, 0.f, 0.f};
 	}
 	float t = -1.f;
 	int indexClosest = -1;
 	for (int index = 0; index < tabSphere.size(); index++) {
-		auto res = RayTracing::intersect(ray, tabSphere[index]);
+		auto res = tabSphere[index].intersect(ray);
 		float value = res.value_or(-1.f);
 		if (value > 0.f && (value < t || t < 0.f)) {
 			t = value;
@@ -67,7 +36,7 @@ Vec3<float> kesseKisePazeOBouDutRaillon(Rayon ray, int profondeur) {
 
 		//si jamais on est sur une sphere qui reflete
 		if (tabSphere[indexClosest].albedo.albedo == 1.f) {
-			Vec3<float> normale = impact - tabSphere[indexClosest].centre;
+			Vec3<float> normale = impact - tabSphere[indexClosest].position;
 			normalize(normale);
 			impact = impact + 0.01 * normale;//on évité l'acnée
 			Vec3<float> moinsI = Vec3<float>{-ray.direction.x,-ray.direction.y ,-ray.direction.z };
@@ -93,7 +62,7 @@ Vec3<float> kesseKisePazeOBouDutRaillon(Rayon ray, int profondeur) {
 
 			//ok on fait le meme rayon que si on faisait du mirroir pour faire les lumières on fera les projections en hémisphere 
 
-			Vec3<float> normale = impact - tabSphere[indexClosest].centre;
+			Vec3<float> normale = impact - tabSphere[indexClosest].position;
 			normalize(normale);
 			impact = impact + 0.01 * normale;//on évité l'acnée
 			Vec3<float> moinsI = Vec3<float>{ -ray.direction.x,-ray.direction.y ,-ray.direction.z };
@@ -126,7 +95,7 @@ Vec3<float> kesseKisePazeOBouDutRaillon(Rayon ray, int profondeur) {
 
 				for (int index = 0; index < tabSphere.size(); index++) {
 
-					auto res2 = RayTracing::intersect(shadowRay, tabSphere[index]);
+					auto res2 = tabSphere[index].intersect(ray);
 					float res2Value = res2.value_or(-1.f);
 					if (bestResult == -1.f) {
 						bestResult = res2Value;
@@ -138,7 +107,7 @@ Vec3<float> kesseKisePazeOBouDutRaillon(Rayon ray, int profondeur) {
 				if (bestResult < 0.f || bestResult>norm(light)) {
 					//On as pas de sphere qui gene notre oeil
 					float norme = norm(light);
-					Vec3<float> normal = impact - tabSphere[indexClosest].centre;
+					Vec3<float> normal = impact - tabSphere[indexClosest].position;
 					normalize(normal);
 					float diffuse = 1.f / (norme * norme) * (dot(normal, shadowRay.direction));
 					if (diffuse < 0) {
@@ -169,7 +138,7 @@ void RayTracing::draw600600() {
 	Lumiere L(Vec3<float>{300.f, 300.f, 100.f}, 0.7f, 0.7f, 0.7f, 30000000);
 	Lumiere L2(Vec3<float>{500.f, 100.f, 100.f}, 0.1f, 0.1f, 0.9f, 30000000);
 
-	tabSphere.push_back(S);
+	//tabSphere.push_back(S);
 	tabSphere.push_back(S2);
 	tabSphere.push_back(S3);
 	tabLumiere.push_back(L);
@@ -198,7 +167,7 @@ void RayTracing::draw600600() {
 			Vec3<float> directionRayon = Vec3<float>{ (float)i, (float)j, 0.f } -pointCamera;
 			normalize(directionRayon);
 			Rayon ray(Vec3<float>{(float)i, (float)j, 0.f}, directionRayon);
-			Vec3<float> finalLight = kesseKisePazeOBouDutRaillon(ray, 0);
+			Vec3<float> finalLight = RayTracing::kesseKisePazeOBouDutRaillon(ray, 0);
 			ppm.pixelMatrix[i][j] = Vector3<int>(std::clamp((int)finalLight.x, 0, 255), std::clamp((int)finalLight.y, 0, 255), std::clamp((int)finalLight.z, 0, 255));
 			 
 		}
--- ProjetTest/ProjetTest/Sphere.cpp
+++ ProjetTest/ProjetTest/Sphere.cpp
@@ -1,13 +1,13 @@
 #include "Sphere.h"
 Sphere::Sphere(Vec3<float> c, float r, Albedo albe) {
-	centre = c;
+	position = c;
 	rayon = r;
 	this->albedo = albe;
 	color = Couleur();
 }
 
 Sphere::Sphere() {
-	this->centre = Vec3<float>{ 0.f, 0.f, 0.f };
+	this->position = Vec3<float>{ 0.f, 0.f, 0.f };
 	this->rayon = 10.f;
 	this->albedo = Albedo();
 	this->color = Couleur();
@@ -15,8 +15,39 @@ Sphere::Sphere() {
 
 Sphere::Sphere(Vec3<float> c, float r, Albedo albe, Couleur col)
 {
-	centre = c;
+	position = c;
 	rayon = r;
 	this->albedo = albe;
 	color = col;
+}
+
+std::optional<float> Sphere::intersect(Rayon R) {
+	float a = 1.f;
+	float b = 2 * (dot(R.origin, R.direction) - dot(position, R.direction));
+	float c = dot(R.origin, R.origin) + dot(position, position) - 2 * dot(position, R.origin) - rayon * rayon;
+	float delta = b * b - 4 * a * c;
+
+	if (delta > 0) {
+		float sol1 = (-b - std::sqrt(delta)) / (2 * a);
+		float sol2 = (-b + std::sqrt(delta)) / (2 * a);
+
+		if (sol1 >= 0.f) {
+			return sol1;
+		}
+		else if (sol2 >= 0.f) {
+			return sol2;
+		}
+		else {
+			//t=sol2;
+			//return sol2 > 0.f;
+			return std::nullopt;
+		}
+	}
+	else if (delta == 0) {
+		float sol = (-b / 2 * a);
+		return sol;
+	}
+	else {
+		return std::nullopt;
+	}
 }
